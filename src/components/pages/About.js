1e435f78176318f58606abbbc33d64c4cceae922
savinu <savinu.vijay@gmail.com>
2021-10-20 21:30:42 +0530

    added demo link to readme

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <Fragment>
             <h1>About This App</h1>
-            <p>Zolve Assignment</p>
+            <p>Zolve Assignments</p>
             <p>Version: 1.0.0</p>
         </Fragment>
     );
